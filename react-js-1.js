8e0f500c9da54b8965aa2261894b79bdfc9df11a
Ruler771 <39794058+Ruler771@users.noreply.github.com>
2018-11-28 22:44:47 +0300

    Update react-js-1.js

--- react-js-1.js
+++ react-js-1.js
@@ -1,1 +1,20 @@
-test
+==== Домашнее задание урок 1 ====
+  
+function loop(times = 0, callback = null){
+  if(typeof(callback) === 'function'){
+    for(let i = 0; i < times; i++){
+     callback();
+    }
+  }
+}
+
+function hello() {
+  console.log('hello world')
+}
+
+loop(1, hello)
+
+==================================
+  
+==== Домашнее задание урок 2 ====
+  
